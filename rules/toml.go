e69464209d6acafc6562dd6eaac219fcf2781867
tobb10001 <tobb10001@gmail.com>
2023-12-16 23:52:25 +0100

    fix trailing period for short captions

--- rules/toml.go
+++ rules/toml.go
@@ -5,13 +5,17 @@ import (
 	"embed"
 	"fmt"
 	"io/fs"
+	"lintex/files"
+	"lintex/tslatex"
 	"path/filepath"
+	"testing"
 
 	"github.com/rs/zerolog/log"
 	sitter "github.com/smacker/go-tree-sitter"
+	"github.com/stretchr/testify/assert"
 )
 
-//go:embed **/*.toml
+//go:embed toml/*
 var vendored_toml embed.FS
 
 type TomlRule struct {
@@ -29,7 +33,7 @@ type TomlRuleTests struct {
 
 type TomlRuleTestCase struct {
 	Name  string
-	Input []byte 
+	Input []byte
 }
 
 func (tr TomlRule) Apply(query *sitter.Query, match *sitter.QueryMatch, _ []byte) (*Range, error) {
@@ -76,4 +80,29 @@ func TomlRulesFromFS(filesystem fs.FS) ([]TomlRule, error) {
 
 func TomlGetVendored() ([]TomlRule, error) {
 	return TomlRulesFromFS(vendored_toml)
+}
+
+func checkInput(t *testing.T, input []byte, rule TomlRule, expectedViolations int) bool {
+	tree, err := tslatex.GetTree(input)
+	if !assert.NoError(t, err) {
+		return false
+	}
+	violations, err := ApplyRule(files.File{Path: "testfile", Tree: tree, Source: input}, rule)
+	if !assert.Equal(t, expectedViolations, len(violations)) {
+		return false
+	}
+	return true
+}
+
+func TestTomlRule(t *testing.T, rule TomlRule) bool {
+	result := true
+	t.Logf("Rule: %s, Obediences: %d, Violations: %d.", rule.Name(), len(rule.Tests().Obediences), len(rule.Tests().Violations))
+
+	for _, obedience := range rule.Tests().Obediences {
+		result = checkInput(t, obedience.Input, rule, 0) && result
+	}
+	for _, violation := range rule.Tests().Violations {
+		result = checkInput(t, violation.Input, rule, 1) && result
+	}
+	return result
 }
--- rules/toml_test.go
+++ rules/toml_test.go
@@ -4,11 +4,8 @@ package rules_test
 import (
 	"testing"
 
-	"lintex/files"
 	"lintex/rules"
-	"lintex/tslatex"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -17,29 +14,6 @@ func TestTomlRules(t *testing.T) {
 	require.NoError(t, err)
 
 	for _, rule := range ruls {
-		t.Run(rule.Name(), func(t *testing.T) {
-			for _, obedience := range rule.Tests().Obediences {
-				t.Run(obedience.Name, func(t *testing.T){
-					tree, err := tslatex.GetTree(obedience.Input)
-					require.NoError(t, err)
-					violations, err := rules.ApplyRule(
-						files.File{Path: "testfile", Tree: tree, Source: obedience.Input},
-						rule,
-					)
-					assert.Equal(t, 0, len(violations), "Expected to see no violation.")
-				})
-			}
-			for _, violation := range rule.Tests().Violations {
-				t.Run(violation.Name, func(t *testing.T) {
-					tree, err := tslatex.GetTree(violation.Input)
-					require.NoError(t, err)
-					violations, err := rules.ApplyRule(
-						files.File{Path: "testfile", Tree: tree, Source: violation.Input},
-						rule,
-					)
-					assert.Equal(t, 1, len(violations), "Expected to see one violation.")
-				})
-			}
-		})
+		t.Run(rule.Name(), func(t *testing.T) { rules.TestTomlRule(t, rule) })
 	}
 }
