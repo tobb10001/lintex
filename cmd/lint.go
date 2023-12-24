78a6ba4d93c0e898ae9561e9cc962bd5d44c96b8
tobb10001 <tobb10001@gmail.com>
2023-12-24 12:36:05 +0100

    add cobra CLI

---
+++ cmd/lint.go
@@ -0,0 +1,61 @@
+package cmd
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+
+	"lintex/files"
+	"lintex/output"
+	"lintex/rules"
+)
+
+func lint(cmd *cobra.Command, args []string) {
+
+	rulez, err := rules.GetRules()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error getting the rules.")
+	}
+
+	log.Debug().Int("len", len(rulez)).Msg("Found rules.")
+
+	files, err := files.GetFiles()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error finding files.")
+	}
+
+	_logArr := zerolog.Arr()
+	for _, file := range files {
+		_logArr.Str(file.Path)
+	}
+	log.Debug().Int("len", len(files)).Array("files", _logArr).Msg("Found files.")
+
+	var violations []rules.Violation
+	for _, file := range files {
+		for _, rule := range rulez {
+			log.Trace().Str("file", file.Path).Str("rule", rule.Name()).Msg("Applying rule to file.")
+
+			ranges, err := rules.ApplyRule(file, rule)
+			if err != nil {
+				log.Warn().Err(err).Str("name", rule.Name()).Msg("Error applying a rule.")
+			}
+
+			log.Trace().Int("len", len(ranges)).Str("rule", rule.Name()).Str("file", file.Path).Msg("Found ranges.")
+
+			for _, rang := range ranges {
+				violations = append(violations, rules.Violation{
+					File: file.Path, Rule: rule, Range: rang, Source: file.Source,
+				})
+			}
+		}
+	}
+
+	log.Debug().Int("len", len(violations)).Msg("Before printing violations.")
+
+	for _, violation := range violations {
+		err := output.PrintRuleViolation(&violation)
+		if err != nil {
+			log.Warn().Err(err).Msg("Failed to print a violation.")
+		}
+	}
+}
---
+++ cmd/root.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	WorkDir string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "lintex",
+	Short: "A linter for LaTeX, powered by Tree-Sitter.",
+	Long:  `To run LinTeX, simply run lintex from your projects root directory.`,
+	PersistentPreRunE: func(_ *cobra.Command, _ []string) error {
+		return os.Chdir(WorkDir)
+	},
+	Run: lint,
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.
+		PersistentFlags().
+		StringVar(&WorkDir, "workdir", "", "specify the working directory")
+}
