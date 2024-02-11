bade873f8c58df8438d9f8bb91616d15dcea9402
tobb10001 <tobb10001@gmail.com>
2024-02-11 16:23:18 +0100

    introduce viper

--- cmd/root.go
+++ cmd/root.go
@@ -4,16 +4,15 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
-
-var WorkDir string
 
 var rootCmd = &cobra.Command{
 	Use:   "lintex",
 	Short: "A linter for LaTeX, powered by Tree-Sitter.",
 	Long:  `To run LinTeX, simply run lintex from your projects root directory.`,
 	PersistentPreRunE: func(_ *cobra.Command, _ []string) error {
-		return os.Chdir(WorkDir)
+		return os.Chdir(viper.GetString("workdir"))
 	},
 	Run: lint,
 }
@@ -28,5 +27,6 @@ func Execute() {
 func init() {
 	rootCmd.
 		PersistentFlags().
-		StringVar(&WorkDir, "workdir", "", "specify the working directory")
+		String("workdir", "", "specify the working directory")
+	_ = viper.BindPFlag("workdir", rootCmd.PersistentFlags().Lookup("workdir"))
 }
